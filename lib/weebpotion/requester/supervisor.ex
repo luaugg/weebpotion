9c9bddefb2e2a7c3e1858ac02631ac62e14f32db
Sam Pritchard <ophisbonklol@gmail.com>
2018-10-31 23:10:15 +0000

    remove requester supervisor (brought back later), add functionality

--- lib/weebpotion/requester/supervisor.ex
+++
@@ -1,5 +0,0 @@
-defmodule WeebPotion.Requester.Supervisor do
-  use Supervisor
-  def start_link(opts), do: Supervisor.start_link(__MODULE__, :ok, opts)
-  def init(:ok), do: Supervisor.init([WeebPotion.Requester], strategy: :one_for_one)
-end
