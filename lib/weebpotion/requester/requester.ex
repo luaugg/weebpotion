de3174a9c50293c2ea64a1391dca49a7e458c4f6
Sam Pritchard <ophisbonklol@gmail.com>
2018-10-31 02:54:23 +0000

    add basic requester

---
+++ lib/weebpotion/requester/requester.ex
@@ -0,0 +1,6 @@
+# very nice and detailed requester :thumbsup:
+
+defmodule WeebPotion.Requester do
+  use HTTPoison.Base
+  def process_request_url(url), do: "https://api.weeb.sh/images/" <> url
+end
