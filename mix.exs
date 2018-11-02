48a681e6899bfeac500efe114d953ba79e57256d
Sam Pritchard <ophisbonklol@gmail.com>
2018-11-02 16:35:28 +0000

    update mix for publishing

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Weebpotion.MixProject do
+defmodule WeebPotion.MixProject do
   use Mix.Project
 
   def project do
@@ -7,22 +7,35 @@ defmodule Weebpotion.MixProject do
       version: "0.2.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :beta,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      name: "weebpotion",
+      source_url: "https://github.com/SamOphis/weebpotion",
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [extra_applications: [:logger]]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:poison, "~> 3.1"},
       {:httpoison, "~> 1.4"},
       {:earmark, "~> 1.2", only: :dev},
       {:ex_doc, "~> 0.19", only: :dev}
+    ]
+  end
+
+  defp description do
+    "Incomplete Elixir wrapper for the Weeb.sh API. Supports most of toph."
+  end
+
+  defp package do
+    [
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/SamOphis/weebpotion"}
     ]
   end
 end
